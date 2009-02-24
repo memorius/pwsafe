07c7768d0c5a0ca117328d590ad7e9db7a999bd8
Nick Clarke <nick.clarke@coretech.co.nz>
2009-02-24 22:58:10 +1300

    New project: added build file, encryption lib, started on main class

---
+++ src/pwsafe/PWSafe.java
@@ -0,0 +1,24 @@
+package pwsafe;
+
+/**
+ * Main entry point class
+ *
+ * @author Nick Clarke
+ */
+public class PWSafe {
+
+    public static void main(final String[] args) {
+        new PWSafe().start();
+    }
+
+    /**
+     * Construct a PWSafe
+     */
+    public PWSafe() {
+        
+    }
+
+    public void start() {
+        
+    }
+}
