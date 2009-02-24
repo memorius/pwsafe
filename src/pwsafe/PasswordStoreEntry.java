bfbb148953fb86473d873a374628a90c18c670fb
Nick Clarke <nick.clarke@coretech.co.nz>
2009-02-25 01:37:09 +1300

    Add PasswordStoreEntry class

---
+++ src/pwsafe/PasswordStoreEntry.java
@@ -0,0 +1,373 @@
+package pwsafe;
+
+import java.io.Externalizable;
+import java.io.InvalidClassException;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.Arrays;
+
+/**
+ * A single account record.
+ * <p>
+ * Note: this class has a natural ordering that is inconsistent with equals.
+ *
+ * @author Nick Clarke
+ */
+public final class PasswordStoreEntry implements Externalizable, Comparable<PasswordStoreEntry> {
+    /**
+     * serialVersionUID for this class.
+     * <p>
+     * For backward-compatibility, do NOT change this when changing serialization implementation:
+     * change VERSION field instead.
+     *
+     * @see #writeExternal(ObjectOutput)
+     */
+    private static final long serialVersionUID = 4653207431849795142L;
+
+    /**
+     * Allows backward-compatible deserialization for this class.
+     * <p>
+     * For backward-compatibility, increment this when changing serialization implementation,
+     * and update readExternal to handle the new and old versions.
+     *
+     * @see #writeExternal(ObjectOutput)
+     * @see #readExternal(ObjectInput)
+     */
+    private static final byte VERSION = 0x1;
+
+
+    private transient boolean _destroyed = false;
+
+    private int _entryID;
+    private String _displayName;
+    private String _userID;
+    private char[] _password;
+    private char[] _additionalInfo;
+
+    /**
+     * Construct a PasswordStoreEntry
+     * <p>
+     * <b>IMPORTANT:</b> this stores references to the supplied password and additionalInfo arrays (if any),
+     * it does not make copies.
+     * The caller should discard their references to these arrays but leave their contents intact;
+     * this PasswordStoreEntry object assumes responsibility for clearing and discarding the secret data.
+     *
+     * @param entryID unique ID for this store entry - invariant across editing and re-saving
+     * @param displayName name to appear in list of entries, typically a website address or company name,
+     *         must not be null or empty
+     * @param userID the account username / login name, can be null or empty
+     * @param password the account password, can be null or empty
+     * @param additionalInfo free-form text to record any extra login info for this account,
+     *         e.g. additional security questions, can be null or empty
+     * @throws IllegalArgumentException if displayName is null or empty
+     */
+    public PasswordStoreEntry(
+            final int entryID,
+            final String displayName,
+            final String userID,
+            final char[] password,
+            final char[] additionalInfo) {
+        checkDisplayName(displayName);
+        _entryID = entryID;
+        _displayName = displayName;
+        _userID = userID;
+        _password = password;
+        _additionalInfo = additionalInfo;
+    }
+
+    /**
+     * @throws IllegalArgumentException if displayName is null or empty
+     */
+    private void checkDisplayName(String displayName) {
+        if (displayName == null) {
+            throw new IllegalArgumentException("displayName must not be null");
+        }
+        if ("".equals(displayName)) {
+            throw new IllegalArgumentException("displayName cannot be an empty string");
+        }
+    }
+
+    /**
+     * Get the unique ID for this store entry
+     *
+     * @return the entry ID
+     */
+    public int getEntryID() {
+        return _entryID;
+    }
+
+    /**
+     * Get the display name for this record, to appear in list of password store entries.
+     * This is typically a website address or company name.
+     *
+     * @return the display name, never null or empty
+     */
+    public String getDisplayName() {
+        return _displayName;
+    }
+
+    /**
+     * Set the display name for this record, to appear in list of password store entries.
+     * This is typically a website address or company name.
+     *
+     * @param displayName the new display name, must not be null or empty
+     * @throws IllegalArgumentException if displayName is null or empty
+     */
+    public void setDisplayName(String displayName) {
+        checkDisplayName(displayName);
+        _displayName = displayName;
+    }
+
+    /**
+     * Get the account username / login name for this record.
+     *
+     * @return the username, can be null or empty
+     */
+    public String getUserID() {
+        return _userID;
+    }
+
+    /**
+     * Set the account username / login name for this record.
+     *
+     * @param userID the new account username / login name, can be null or empty
+     */
+    public void setUserID(String userID) {
+        _userID = userID;
+    }
+
+    /**
+     * Get the account password (secret) for this record, if any.
+     * <p>
+     * <b>IMPORTANT:</b> this returns the internal array itself, not a copy,
+     * so any modifications to the array will be written back to the store with this entry.
+     * <p>
+     * For security, caller should avoid storing references longer than necessary,
+     * and if the caller copies the data, it MUST ensure it is zero-overwritten and discarded when finished.
+     * <p>
+     * Assuming the caller does not make a copy, this PasswordStoreEntry retains responsibility for clearing
+     * and discarding the secret data, and caller should not zero it.
+     *
+     * @return the password (plaintext), can be null or empty.
+     * @throws IllegalStateException if {@link #destroy()} method has been called
+     */
+    public char[] getPassword() {
+        if (_destroyed) {
+            throw new IllegalStateException("destroy() has already been called");
+        }
+        return _password;
+    }
+
+    /**
+     * Set the account password (secret) for this record, if any.
+     * The existing password (if any) will be zeroed and discarded.
+     * <p>
+     * <b>IMPORTANT:</b> this stores a reference to the supplied array, it does not make a copy.
+     * The caller should discard their reference to it but leave its contents intact;
+     * this PasswordStoreEntry object assumes responsibility for clearing and discarding the secret data.
+     *
+     * @param password the new password (plaintext), can be null or empty
+     * @throws IllegalStateException if {@link #destroy()} method has been called
+     */
+    public void setPassword(char[] password) {
+        if (_destroyed) {
+            throw new IllegalStateException("destroy() has already been called");
+        }
+        clearPassword();
+        _password = password;
+    }
+
+    /**
+     * Get the additional login info (secret) for this record, if any.
+     * <p>
+     * <b>IMPORTANT:</b> this returns the internal array itself, not a copy,
+     * so any modifications to the array will be written back to the store with this entry.
+     * <p>
+     * For security, caller should avoid storing references longer than necessary,
+     * and if the caller copies the data, it MUST ensure it is zero-overwritten and discarded when finished.
+     * <p>
+     * Assuming the caller does not make a copy, this PasswordStoreEntry retains responsibility for clearing
+     * and discarding the secret data, and caller should not zero it.
+     *
+     * @return the additional login info (plaintext), can be null or empty.
+     * @throws IllegalStateException if {@link #destroy()} method has been called
+     */
+    public char[] getAdditionalInfo() {
+        if (_destroyed) {
+            throw new IllegalStateException("destroy() has already been called");
+        }
+        return _additionalInfo;
+    }
+
+    /**
+     * Set the additional login info (secret) for this record, if any.
+     * The existing additional login info (if any) will be zeroed and discarded.
+     * <p>
+     * <b>IMPORTANT:</b> this stores a reference to the supplied array, it does not make a copy.
+     * The caller should discard their reference to it but leave its contents intact;
+     * this PasswordStoreEntry object assumes responsibility for clearing and discarding the secret data.
+     *
+     * @param additionalInfo the new additional login info (plaintext), can be null or empty
+     * @throws IllegalStateException if {@link #destroy()} method has been called
+     */
+    public void setAdditionalInfo(char[] additionalInfo) {
+        if (_destroyed) {
+            throw new IllegalStateException("destroy() has already been called");
+        }
+        clearAdditionalInfo();
+        _additionalInfo = additionalInfo;
+    }
+
+    /**
+     * Check whether another object is a PasswordStoreEntry (not a subclass) and represents the same account as
+     * this object. Two objects are for the same account if they have the same entryID.
+     *
+     * @param o the object to compare to this object
+     * @return true if o is equal to this object (is a PasswordStoreEntry with the same entryID)
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PasswordStoreEntry other = (PasswordStoreEntry) o;
+        return _entryID == other._entryID;
+    }
+
+    /**
+     * Generate a hash code for this object.
+     *
+     * @return the hash code, which considers only the entryID field
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        final int init = 4708;
+        int hash = init;
+        hash = prime * hash + (int) _entryID;
+        return hash;
+    }
+
+    /**
+     * Sort by _displayName then by _userID.
+     * <p>
+     * Note: this class has a natural ordering that is inconsistent with equals.
+     */
+    public int compareTo(PasswordStoreEntry other) {
+        if (this == other) {
+            return 0;
+        }
+        // Sort by display name
+        int compare = _displayName.compareTo(other._displayName);
+        if (compare != 0) {
+            return compare;
+        }
+        // Same displayname - sort by userID; null entries considered greater
+        String otherUserID = other._userID;
+        if (otherUserID != null) {
+            if (_userID != null) {
+                compare = _userID.compareTo(otherUserID);
+                if (compare != 0) {
+                    return compare;
+                }
+            } else {
+                return 1;
+            }
+        } else {
+            if (_userID != null) {
+                return -1;
+            }
+        }
+        // Tiebreaker
+        if (_entryID < other._entryID) {
+            return -1;
+        }
+        if (_entryID > other._entryID) {
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Zero-overwrite and discard the secret password field, if any.
+     */
+    private void clearPassword() {
+        if (_password != null) {
+            Arrays.fill(_password, (char) 0);
+            _password = null;
+        }
+    }
+
+    /**
+     * Zero-overwrite and discard the secret additional-info field, if any.
+     */
+    private void clearAdditionalInfo() {
+        if (_additionalInfo != null) {
+            Arrays.fill(_additionalInfo, (char) 0);
+            _additionalInfo = null;
+        }
+    }
+
+    /**
+     * Zero-overwrite and discard the secret password and additional-info fields, if any.
+     * This method can safely be called repeatedly.
+     * Once called, getPassword / getAdditionalInfo methods are no longer available.
+     */
+    public void destroy() {
+        clearPassword();
+        clearAdditionalInfo();
+        _destroyed = true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        destroy();
+    }
+
+    /**
+     * Explicit serialization to guarantee we can handle old versions if implementation evolves
+     */
+    public void writeExternal(ObjectOutput out) throws IOException {
+        if (_destroyed) {
+            throw new NotSerializableException(getClass().getName() + " : destroy() has already been called");
+        }
+        out.writeByte(VERSION);
+        /* For backward-compatible deserialization, change only the part below, and change VERSION value at top of file,
+           and change readExternal to support both old and new */
+        out.writeInt(_entryID);
+        out.writeObject(_displayName);
+        out.writeObject(_userID);
+        out.writeObject(_password);
+        out.writeObject(_additionalInfo);
+    }
+
+    /**
+     * Explicit deserialization to guarantee we can handle old versions if implementation evolves
+     */
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        byte version = in.readByte();
+        // Add new versions here when changing writeExternal / VERSION field
+        switch (version) {
+            case 0x1:
+                readExternalVersion1(in);
+                break;
+            default:
+                throw new InvalidClassException(getClass().getName(),
+                        "The VERSION '" + version + "' was read from the stream but is not supported by the readExternal implementation");
+        }
+    }
+
+    private void readExternalVersion1(ObjectInput in) throws IOException, ClassNotFoundException {
+        _destroyed = false;
+        _entryID = in.readInt();
+        _displayName    = (String) in.readObject();
+        _userID         = (String) in.readObject();
+        _password       = (char[]) in.readObject();
+        _additionalInfo = (char[]) in.readObject();
+    }
+}
