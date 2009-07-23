bf667f7caae97fb6225761868e56663a1d66172d
Nick Clarke <nick.clarke@coretech.co.nz>
2009-07-23 22:38:00 +1200

    Fix quoting in install.sh, explicitly specify JVM

--- install.sh
+++ install.sh
@@ -8,32 +8,35 @@ fi
 PROG_NAME=pwsafe
 DIST_JAR_DIR=dist
 
-INSTALLED_BIN_DIR=/usr/local/bin
-INSTALLED_JAR_DIR=/usr/local/share/${PROG_NAME}
+# JAVA_RUNTIME=java
+JAVA_RUNTIME=/usr/lib/jvm/java-6-sun/bin/java
 
-DIST_JAR=${DIST_JAR_DIR}/${PROG_NAME}.jar
-INSTALLED_BIN_SCRIPT=${INSTALLED_BIN_DIR}/${PROG_NAME}
+INSTALLED_BIN_DIR=/usr/local/bin
+INSTALLED_JAR_DIR="/usr/local/share/${PROG_NAME}"
+
+DIST_JAR="${DIST_JAR_DIR}/${PROG_NAME}.jar"
+INSTALLED_BIN_SCRIPT="${INSTALLED_BIN_DIR}/${PROG_NAME}"
 
 if [ ! -f "${DIST_JAR}" ]; then
     echo "File '${DIST_JAR}' does not exist - please run 'ant dist' first."
     exit 1
 fi
 
-mkdir -p ${INSTALLED_JAR_DIR}
-mkdir -p ${INSTALLED_BIN_DIR}
+mkdir -p "${INSTALLED_JAR_DIR}"
+mkdir -p "${INSTALLED_BIN_DIR}"
 
-cp -r ${DIST_JAR_DIR}/* ${INSTALLED_JAR_DIR}
-chown -R root:root ${INSTALLED_JAR_DIR}
-chmod -R a=rX,u+w ${INSTALLED_JAR_DIR}
+cp -r "${DIST_JAR_DIR}"/* "${INSTALLED_JAR_DIR}"
+chown -R root:root "${INSTALLED_JAR_DIR}"
+chmod -R a=rX,u+w "${INSTALLED_JAR_DIR}"
 
-cat > ${INSTALLED_BIN_SCRIPT} <<-END
+cat > "${INSTALLED_BIN_SCRIPT}" <<-END
 	#!/bin/bash
 	
-	exec java -jar ${INSTALLED_JAR_DIR}/${PROG_NAME}.jar "\$@"
+	exec "$JAVA_RUNTIME" -jar "${INSTALLED_JAR_DIR}/${PROG_NAME}.jar" "\$@"
 	END
 
-chown root:root ${INSTALLED_BIN_SCRIPT}
-chmod a=rx,u+w ${INSTALLED_BIN_SCRIPT}
+chown root:root "${INSTALLED_BIN_SCRIPT}"
+chmod a=rx,u+w "${INSTALLED_BIN_SCRIPT}"
 
 echo "Install finished ok."
 echo "Run '${INSTALLED_BIN_SCRIPT}' (or just '${PROG_NAME}') to start the program."
